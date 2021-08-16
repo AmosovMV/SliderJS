adbaf8d695a55cf61c9927c0ac3155457df3f22d
Maksim <82663071+AmosovMV@users.noreply.github.com>
2021-08-16 15:29:25 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let imageArray = [{ src: "./asset/58.jpg" }, { src: "./asset/41.jpg" }];
+
+function initImageSlider() {
+  let slider_img = document.querySelector(".slider_img");
+  let point_select = document.querySelector('.point-select');
+
+
+  
+  function initImage() {
+    imageArray.forEach(function(item,index){
+      let imgDiv = `<div class="image_${index} ${index===0? "currentImg":""}" style="backgroung-image:src(${imageArray[index].src})"></div>`;
+      slider_img.innerHTML += imgDiv;
+    })
+  };
+}
+
+document.addEventListener("DOMContentLoaded", initImageSlider);
