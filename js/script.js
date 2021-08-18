33184b5021aecb307aef2254cb943e255fd35845
Maksim <82663071+AmosovMV@users.noreply.github.com>
2021-08-18 10:47:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
-let imageArray = [{ url: "./asset/58.jpg" }, { url: "./asset/41.jpg" }];
+let imageArray = [
+  {
+    url: './asset/58.jpg',
+    title: "Рис1"
+  },
+  {
+    url: './asset/41.jpg',
+    title: "Рис2"
+  },
+];
+
 
 function initImageSlider() {
   let slider_img = document.querySelector(".slider_img");
   let point_select = document.querySelector('.point-select');
 
 
-  
+
   function initImage() {
-    imageArray.forEach(function(item,index){
-      let imgDiv = `<div class="img_${index} image ${index === 0? "currentImg" : ""}" style=background-image:url(${imageArray[index].url})></div>`;
+    imageArray.forEach(function (item, index) {
+      let imgDiv = `<div class="img_${index} image ${index === 0 ? "currentImg" : ""}" style=background-image:url(${imageArray[index].url})></div>`;
       slider_img.innerHTML += imgDiv;
     })
   };
 
   initImage();
-  
+
 };
 
 document.addEventListener("DOMContentLoaded", initImageSlider);
