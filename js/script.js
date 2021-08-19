eda1acf810700a648ce59976e31cdc4cbe1e9d2c
Maksim <82663071+AmosovMV@users.noreply.github.com>
2021-08-19 15:42:16 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,23 +7,48 @@ let imageArray = [
     url: './asset/41.jpg',
     title: "Рис2"
   },
+  {
+    url: './asset/43.jpg',
+    title: "Рис3"
+  },
 ];
 
 
 function initImageSlider() {
   let slider_img = document.querySelector(".slider_img");
-  let point_select = document.querySelector('.point-select');
+  let point_select_left = document.querySelector('.point-select-left');
+  let point_select_right = document.querySelector('.point-select-right');
 
 
 
   function initImage() {
     imageArray.forEach(function (item, index) {
-      let imgDiv = `<div class="img_${index} image ${index === 0 ? "currentImg" : ""}" style=background-image:url(${imageArray[index].url})></div>`;
+      let imgDiv = `<div class="img${index} image ${index === 0 ? "currentImg" : ""}" style=background-image:url(${imageArray[index].url}) data-index=${index}></div>`;
       slider_img.innerHTML += imgDiv;
     })
   };
 
   initImage();
+
+  function initSelect() {
+    let currentIndex = +slider_img.querySelector(".currentImg").dataset.index;
+    let nextIndex=0;
+    point_select_right.addEventListener('click', function () {
+      nextIndex === imageArray.length-1 ? nextIndex = 0 : nextIndex += 1;
+      moveCurrentImg(nextIndex);
+    })
+    point_select_left.addEventListener('click', function () {
+      nextIndex === 0 ? nextIndex = imageArray.length-1 : nextIndex -= 1;
+      moveCurrentImg(nextIndex);
+    })
+  }
+
+  initSelect();
+
+  function moveCurrentImg(numb) {
+    slider_img.querySelector('.currentImg').classList.remove("currentImg");
+    slider_img.querySelector('.img' + numb).classList.add("currentImg");
+  };
 
 };
 
